9c077ba2926fc1630f701bea07abefc9d20c6a78
“Daisy <daisyding666@gmail.com>
2024-08-03 10:57:10 -0700

    first commit

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,101 @@
+export default [
+  {
+    _id: "A101",
+    title: "Propulsion Assignment",
+    course: "RS101",
+    due: "2024-05-13",
+    available_from: "2024-05-06",
+    available_to: "2024-05-13",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+  {
+    _id: "A102",
+    title: "Combustion Analysis",
+    course: "RS101",
+    due: "2024-05-20",
+    available_from: "2024-05-13",
+    available_to: "2024-05-20",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+  {
+    _id: "A103",
+    title: "Nozzle Design Project",
+    course: "RS101",
+    due: "2024-05-27",
+    available_from: "2024-05-20",
+    available_to: "2024-05-27",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+  {
+    _id: "A201",
+    title: "Aerodynamics Quiz",
+    course: "RS102",
+    due: "2024-05-13",
+    available_from: "2024-05-06",
+    available_to: "2024-05-13",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+  {
+    _id: "A202",
+    title: "Flow Analysis",
+    course: "RS102",
+    due: "2024-05-20",
+    available_from: "2024-05-13",
+    available_to: "2024-05-20",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+  {
+    _id: "A203",
+    title: "Heating Analysis",
+    course: "RS102",
+    due: "2024-05-27",
+    available_from: "2024-05-20",
+    available_to: "2024-05-27",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+  {
+    _id: "A301",
+    title: "Structural Design Task",
+    course: "RS103",
+    due: "2024-05-13",
+    available_from: "2024-05-06",
+    available_to: "2024-05-13",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+  {
+    _id: "A302",
+    title: "Orbital Calculations",
+    course: "RS103",
+    due: "2024-05-20",
+    available_from: "2024-05-13",
+    available_to: "2024-05-20",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+  {
+    _id: "A303",
+    title: "Systems Engineering Exam",
+    course: "RS103",
+    due: "2024-05-27",
+    available_from: "2024-05-20",
+    available_to: "2024-05-27",
+    points: "100",
+    description:
+      "Submit a link to the landing page of your Web application running on Netlify.",
+  },
+];
---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,339 @@
+export default [
+  {
+    _id: "M101",
+    name: "Introduction to Rocket Propulsion",
+    description: "Basic principles of rocket propulsion and rocket engines.",
+    course: "RS101",
+    lessons: [
+      {
+        _id: "L101",
+        name: "History of Rocketry",
+        description: "A brief history of rocketry and space exploration.",
+        module: "M101",
+      },
+      {
+        _id: "L102",
+        name: "Rocket Propulsion Fundamentals",
+        description: "Basic principles of rocket propulsion.",
+        module: "M101",
+      },
+      {
+        _id: "L103",
+        name: "Rocket Engine Types",
+        description: "Overview of different types of rocket engines.",
+        module: "M101",
+      },
+    ],
+  },
+  {
+    _id: "M102",
+    name: "Fuel and Combustion",
+    description:
+      "Understanding rocket fuel, combustion processes, and efficiency.",
+    course: "RS101",
+    lessons: [
+      {
+        _id: "L201",
+        name: "Rocket Fuel",
+        description: "Overview of different types of rocket fuels.",
+        module: "M102",
+      },
+      {
+        _id: "L202",
+        name: "Combustion Processes",
+        description: "Understanding combustion processes and efficiency.",
+        module: "M102",
+      },
+      {
+        _id: "L203",
+        name: "Combustion Instability",
+        description: "Understanding combustion instability and mitigation.",
+        module: "M102",
+      },
+    ],
+  },
+  {
+    _id: "M103",
+    name: "Nozzle Design",
+    description:
+      "Principles of rocket nozzle design and performance optimization.",
+    course: "RS101",
+    lessons: [
+      {
+        _id: "L301",
+        name: "Nozzle Design",
+        description: "Overview of different types of rocket nozzles.",
+        module: "M103",
+      },
+      {
+        _id: "L302",
+        name: "Nozzle Performance",
+        description: "Understanding nozzle performance and efficiency.",
+        module: "M103",
+      },
+      {
+        _id: "L303",
+        name: "Nozzle Optimization",
+        description: "Optimizing nozzle design for specific applications.",
+        module: "M103",
+      },
+    ],
+  },
+  {
+    _id: "M201",
+    name: "Fundamentals of Aerodynamics",
+    description: "Basic aerodynamic concepts and fluid dynamics principles.",
+    course: "RS102",
+    lessons: [
+      {
+        _id: "L201",
+        name: "Introduction to Aerodynamics",
+        description: "An overview of the fundamental concepts in aerodynamics.",
+        module: "M201",
+      },
+      {
+        _id: "L202",
+        name: "Fluid Dynamics Principles",
+        description:
+          "Basic principles of fluid dynamics as applied to aerodynamics.",
+        module: "M201",
+      },
+      {
+        _id: "L203",
+        name: "Aerodynamic Forces",
+        description:
+          "Understanding the forces acting on an object moving through the air.",
+        module: "M201",
+      },
+    ],
+  },
+  {
+    _id: "M202",
+    name: "Subsonic and Supersonic Flow",
+    description: "Understanding subsonic and supersonic aerodynamic behaviors.",
+    course: "RS102",
+    lessons: [
+      {
+        _id: "L204",
+        name: "Introduction to Subsonic Flow",
+        description: "Basics of subsonic flow and its characteristics.",
+        module: "M202",
+      },
+      {
+        _id: "L205",
+        name: "Introduction to Supersonic Flow",
+        description:
+          "Fundamentals of supersonic flow and its unique properties.",
+        module: "M202",
+      },
+      {
+        _id: "L206",
+        name: "Comparative Analysis of Subsonic and Supersonic Flow",
+        description:
+          "A comparative study of subsonic and supersonic aerodynamic behaviors.",
+        module: "M202",
+      },
+    ],
+  },
+  {
+    _id: "M203",
+    name: "Aerodynamic Heating",
+    description: "Study of aerodynamic heating and thermal protection systems.",
+    course: "RS102",
+    lessons: [
+      {
+        _id: "L207",
+        name: "Basics of Aerodynamic Heating",
+        description: "Introduction to the principles of aerodynamic heating.",
+        module: "M203",
+      },
+      {
+        _id: "L208",
+        name: "Thermal Protection Systems",
+        description:
+          "Overview of thermal protection systems used in aerospace applications.",
+        module: "M203",
+      },
+      {
+        _id: "L209",
+        name: "Materials for Thermal Protection",
+        description:
+          "Study of materials used in thermal protection systems and their properties.",
+        module: "M203",
+      },
+    ],
+  },
+  {
+    _id: "M301",
+    name: "Spacecraft Structural Design",
+    description:
+      "Fundamentals of designing spacecraft structures and materials selection.",
+    course: "RS103",
+    lessons: [
+      {
+        _id: "L301",
+        name: "Introduction to Spacecraft Structures",
+        description:
+          "Basics of spacecraft structural design and the challenges involved.",
+        module: "M301",
+      },
+      {
+        _id: "L302",
+        name: "Materials Selection for Spacecraft",
+        description:
+          "Criteria and processes for selecting materials for spacecraft structures.",
+        module: "M301",
+      },
+      {
+        _id: "L303",
+        name: "Structural Analysis Techniques",
+        description:
+          "Techniques and tools for analyzing spacecraft structures.",
+        module: "M301",
+      },
+    ],
+  },
+  {
+    _id: "M302",
+    name: "Orbital Mechanics",
+    description: "Understanding orbital dynamics and mission planning.",
+    course: "RS103",
+    lessons: [
+      {
+        _id: "L304",
+        name: "Fundamentals of Orbital Mechanics",
+        description:
+          "Basic principles and equations governing orbital mechanics.",
+        module: "M302",
+      },
+      {
+        _id: "L305",
+        name: "Orbital Maneuvers",
+        description: "Techniques for performing orbital maneuvers and changes.",
+        module: "M302",
+      },
+      {
+        _id: "L306",
+        name: "Mission Planning",
+        description:
+          "Planning and executing space missions with a focus on orbital dynamics.",
+        module: "M302",
+      },
+    ],
+  },
+  {
+    _id: "M303",
+    name: "Spacecraft Systems Engineering",
+    description: "Overview of spacecraft systems and subsystems engineering.",
+    course: "RS103",
+    lessons: [
+      {
+        _id: "L307",
+        name: "Introduction to Spacecraft Systems",
+        description:
+          "Overview of the various systems that make up a spacecraft.",
+        module: "M303",
+      },
+      {
+        _id: "L308",
+        name: "Subsystems Engineering",
+        description:
+          "Detailed look into the subsystems of a spacecraft and their functions.",
+        module: "M303",
+      },
+      {
+        _id: "L309",
+        name: "Integration and Testing",
+        description:
+          "Processes for integrating and testing spacecraft systems.",
+        module: "M303",
+      },
+    ],
+  },
+  {
+    _id: "M401",
+    name: "Organic Chemistry I",
+    description:
+      "Introduction to the principles and reactions of organic chemistry.",
+    course: "RS104",
+    lessons: [
+      {
+        _id: "L101",
+        name: "Introduction to Organic Chemistry",
+        description: "Basic concepts and history of organic chemistry.",
+        module: "M401",
+      },
+      {
+        _id: "L102",
+        name: "Hydrocarbons",
+        description:
+          "Study of the structure, properties, and reactions of hydrocarbons.",
+        module: "M401",
+      },
+      {
+        _id: "L103",
+        name: "Functional Groups",
+        description:
+          "Overview of different functional groups in organic molecules and their reactions.",
+        module: "M401",
+      },
+    ],
+  },
+  {
+    _id: "M402",
+    name: "Organic Chemistry II",
+    description: "Detailed study of organic reactions and mechanisms.",
+    course: "RS104",
+    lessons: [
+      {
+        _id: "L104",
+        name: "Organic Reaction Mechanisms",
+        description:
+          "In-depth analysis of various organic reaction mechanisms.",
+        module: "M402",
+      },
+      {
+        _id: "L105",
+        name: "Stereochemistry",
+        description:
+          "Understanding the spatial arrangement of atoms in organic molecules.",
+        module: "M402",
+      },
+      {
+        _id: "L106",
+        name: "Organic Synthesis",
+        description:
+          "Principles and strategies for the synthesis of organic compounds.",
+        module: "M402",
+      },
+    ],
+  },
+  {
+    _id: "M403",
+    name: "Advanced Organic Chemistry",
+    description: "Advanced topics in organic chemistry and applications.",
+    course: "RS104",
+    lessons: [
+      {
+        _id: "L107",
+        name: "Advanced Organic Reactions",
+        description:
+          "Study of advanced organic reactions and their applications.",
+        module: "M403",
+      },
+      {
+        _id: "L108",
+        name: "Bioorganic Chemistry",
+        description: "Intersection of organic chemistry and biochemistry.",
+        module: "M403",
+      },
+      {
+        _id: "L109",
+        name: "Medicinal Chemistry",
+        description:
+          "Application of organic chemistry principles in drug discovery and development.",
+        module: "M403",
+      },
+    ],
+  },
+];
